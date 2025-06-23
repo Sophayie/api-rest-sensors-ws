8366e08561f18d67abd1140460343c0fce5d2fef
Sophayie <157664011+Sophayie@users.noreply.github.com>
2025-06-23 15:59:48 -0400

    Update final avant le transfère sur le main

--- frontend/frontend_angular/src/app/accueil2/accueil2.component.ts
+++ frontend/frontend_angular/src/app/accueil2/accueil2.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
 
 import { SerreComponent } from '../serre/serre.component';
 import { HeaderComponent } from '../header/header.component';
 import { BackgroundFormulaireComponent } from '../background-formulaire/background-formulaire.component';
 import { FormulaireConnexionComponent } from '../formulaire-connexion/formulaire-connexion.component';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-accueil2',
   standalone: true,
   imports: [HeaderComponent,FormulaireConnexionComponent ,SerreComponent,BackgroundFormulaireComponent],
   templateUrl: './accueil2.component.html',
-  styleUrls: ['./accueil2.component.css']
+  styleUrls: ['./accueil2.component.css'],
+  encapsulation: ViewEncapsulation.None,
+
 })
 export class Accueil2Component {
 
